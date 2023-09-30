2a0fc650291d926331f0def7c715123f31b5dfae
shivapabbu1 <93253969+shivapabbu1@users.noreply.github.com>
2023-09-30 11:27:32 +0530

    Add files via upload

---
+++ Patterns/Assignment.java
@@ -0,0 +1,32 @@
+package Patterns;
+import java.lang.Math;
+public class Assignment {
+
+	public static void main(String[] args) {
+		// problem-1:
+	   /*     r1 r2 r3 r4 r5
+	    *  c1 1                   :rpow c  
+		   c2 1  2                :rpow c
+		   c3 1  3  9 
+		   c4 1  4  16  64
+		   c5 1  5  25  125  625
+		         note: any power to zero is 1
+		 /* 
+		    for(int r=1;r<=5;r++) {                     
+			  for(int c=0;c<=(r-1);c++) {
+				 double a=(Math.pow(r,c));
+				 System.out.print(Math.round(a)+" ");
+			    }
+			  System.out.println(" ");
+		     }
+		 */
+		
+		
+		
+		    
+		    
+		
+	
+		
+	}
+}
---
+++ Patterns/number.java
@@ -0,0 +1,153 @@
+package Patterns;
+
+public class number {
+
+public static void main(String[] args) {
+	/*    1 
+		  1 2
+		  1 2 3
+		  1 2 3 4
+		  1 2 3 4 5 
+		*/
+		  
+	/*	 int n=5;
+		 for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+			  System.out.print(j+" ");
+			 }
+			System.out.println();
+		}
+     /*
+	//------------------------------------------
+	/*
+		 * 1
+		 * 2 1
+		 * 3 2 1
+		 * 4 3 2 1
+		 * 5 4 3 2 1
+		 * 
+	 */
+	
+	/*  int n=5;
+		for(int r=1;r<=n;r++) {
+			for(int c=r;c>=1;c--) {
+				System.out.print(c+" ");
+			}
+			System.out.println();
+		}
+	*/
+	//_________________________________________________
+	/*
+		 * 5
+		 * 4 4
+		 * 3 3 3 
+		 * 2 2 2 2
+		 * 1 1 1 1 1
+		 * 
+	  */
+	
+   /*		 int n=5;
+		for(int r=1;r<=;r++,v--) {
+			
+			for(int c=1;c<=r;c++) {
+				System.out.print(v+" ");
+			}
+			System.out.println();
+	*/
+//-----------------------------------		
+		/*
+			 * 1
+			 * 1 0
+			 * 1 0 1 
+			 * 1 0 1 0
+			 * 1 0 1 0 1
+			 * 
+		 */  
+	/*	int n=6;
+		    for(int r=1;r<n;r++) {
+		    	 for(int c=1;c<=r;c++) {
+		    		 if (c%2==0 ) {
+		    			 System.out.print(0+" ");
+		    		 } else {
+		    			 System.out.print(1+" ");
+		    		 }
+		    		
+		    		 }
+		    	 
+		    	 System.out.println(" ");
+		    }
+		  */
+	
+		//secong logic
+		
+	/*	int n=6;
+	    for(int r=1;r<n;r++) {
+	    	 for(int c=1;c<=r;c++) {
+	    		 System.out.print(((c+r)%2)==0?1:0);
+	    	 }
+	    	 System.out.println("");
+	    }
+	  */ 
+//___________________________________________________________		    
+		    
+		/*
+		 * 1
+		 * 0 1
+		 * 0 1 0 
+		 * 1 0 1 0
+		 * 1 0 1 0 1
+		 * 
+		 */    
+		
+		
+	/*	int n=6;
+		int d=1;
+	    for(int r=1;r<n;r++) {
+	    	 for(int c=1;c<=r;c++) {
+	    		 if(d==1) {
+	    			 d=0;
+	    			 System.out.print(0+" ");
+	    		 }else {
+	    			 d=1;
+	    			System.out.print(1+" "); 
+	    		 }
+	    		
+	    		 }
+	    	 
+	    	 System.out.println(" ");
+	    }
+	  */
+		
+//_________________________
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}}
